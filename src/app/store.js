bb07c9e24d5882b47f4bac4b013a9307a44e2140
iii <miselrkba@gmail.com>
2020-11-08 19:23:31 +0100

    first commit

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import postReducer from '../features/posts/postsSlice'
+
+export default configureStore({
+  reducer: () => ({
+    reducer: {
+      posts: postReducer,
+    },
+  }),
+})
